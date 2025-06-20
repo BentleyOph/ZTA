f0380960d1dd7669d5b600d8515e4f809ac54e14
OB <82271531+BentleyOph@users.noreply.github.com>
2025-06-20 13:34:47 +0300

    chore: fixes

--- ZeroTrustWebUI/email_constants.py
+++ ZeroTrustWebUI/email_constants.py
@@ -1,4 +1,4 @@
 email_sender = 'bentenzz001@gmail.com'
-email_password = 'lnqn bmzx vgys pcoo'
+email_password = 'lnqn bmzx vgys pcod'
 subject = "Privileged Access Management (PAM) Request Approval"
 
